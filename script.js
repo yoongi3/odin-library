40506ce71b780ceb212cc6be69eee4a598d3e56f
Josh <lolhongy3@gmail.com>
2023-06-11 10:23:41 +1000

    add functions to create new books

--- script.js
+++ script.js
@@ -9,40 +9,47 @@ function Book(title, author, pages, read){
     this.read = read
 }
 
-// adding books manually for testing *remove later
-
-const book1 = new Book('book 1','author 1','1 page','not read')
-const book2 = new Book('book 2','author 2','2 pages','read')
-const book3 = new Book('hello','joshy','123 pages', 'read')
-
-AddBookToLibrary(book1);
-AddBookToLibrary(book2);
-AddBookToLibrary(book3)
-
 // DOM Elements
 
-const library = document.getElementById("library");
+const booksGrid = document.getElementById("booksGrid");
+
+const addBookForm = document.getElementById("addBookForm")
 
 // 
 
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const read = document.getElementById('read').value
+    return new Book(title, author, pages, read)
+
+}
+
+const addBook = (e) =>{
+    e.preventDefault()
+    const newBook = getBookFromInput();
+    AddBookToLibrary(newBook);
+    addBookForm.reset();
+}
+
 function AddBookToLibrary(book){
     myLibrary.push(book)
-}
 
-document.body.onload = displayLibrary;
-
-function displayLibrary(){
-    myLibrary.forEach(book => {
-
-        const newDiv = document.createElement("div");
-        newDiv.setAttribute("class","book");
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class","book");
         
-        Object.keys(book).forEach(key => {
-            const p = document.createElement("p");
-            newDiv.appendChild(p);
-            p.appendChild(document.createTextNode(book[key]))
-        });
-
-        library.appendChild(newDiv)
+    Object.keys(book).forEach(key => {
+        const p = document.createElement("p");
+        newDiv.appendChild(p);
+        p.appendChild(document.createTextNode(book[key]))
     });
+
+    booksGrid.appendChild(newDiv)
 }
+
+// Buttons
+
+addBookForm.onsubmit = addBook
+
+
